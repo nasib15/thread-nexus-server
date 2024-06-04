25ad7ffe991a1f87348fec1191a85e09a865c499
knh-nehal <nasibhossain57@gmail.com>
2024-06-04 18:45:42 +0600

    setup mongodb

--- index.js
+++ index.js
@@ -28,6 +28,33 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
+// MongoDB Connection
+// Connection URI
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.talr0yk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
